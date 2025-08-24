b98724eebecef7e62f7c76cb0aecc302458d49e4
Revoj-a <revojjoever@gmail.com>
2025-08-24 11:53:03 +0800

    second commit

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const square = (x) => multiply(x, x);
 const isRightTriangle = (a, b, c) => {
   return square(a) + square(b) === square(c);
 };
-
+console.log("BEFORE");
 isRightTriangle(3, 4, 5);
+console.log("DONE");
